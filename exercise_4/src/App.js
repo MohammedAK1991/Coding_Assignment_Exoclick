b00ec47ca7e317001e66bfe0513bed763c659b31
Mohammed <mohmed.ak.17@gmail.com>
2020-09-24 21:55:24 +0200

    feat/form to input username & repository name

--- exercise_4/src/App.js
+++ exercise_4/src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Form } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
+
+  const [username,setUserName] = useState('MohammedAK1991');
+  const [repository, setRepository] = useState('PARTY-FORECAST')
+
+  function handleChange() {
+
+  }
+
+  function handleSubmit() {
+
+  }
+
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <div className="navbar">
+        Find contributors to a github repo
+      </div>
+      <div className="search">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              placeholder='Name'
+              name='username'
+              value={username}
+              onChange={handleChange}
+            />
+            <Form.Input
+              placeholder='Email'
+              name='repository'
+              value={repository}
+              onChange={handleChange}
+            />
+            <Form.Button content='Submit' />
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 }
